5b4cccdb82b61e77ebdda8cffab059cf6a4ecbbc
Lucas <luve20ac@student.cbs.dk>
2020-10-12 22:57:17 +0200

    isAuthorized working

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var app = express();
 const jwt = require('jsonwebtoken');
 const fs = require('fs')
 
-app.get('/secret', (req,res) => {
+app.get('/secret', isAuthorized, (req,res) => {
     res.json({"message":"This is super secret, do not share!"})
 })
 
@@ -20,9 +20,27 @@ app.get('/readme', (req,res) => {
 
 app.get('/jwt',(req,res) => {
     let privateKey = fs.readFileSync('./private.pem','utf8');
-    let token = jwt.sign({"body": "stuff"}, "MySuperSecretPassPhrase",{algorithm: 'HS256'});
+    let token = jwt.sign({"body": "stuff"}, privateKey ,{algorithm: 'HS256'});
     res.send(token);
 })
+
+function isAuthorized(req, res, next){ // this function gives access with the JWT.
+    if (typeof req.headers.authorization !== "undefined"){
+        let token = req.headers.authorization.split(" ")[1];
+        let privateKey = fs.readFileSync('./private.pem', 'utf8');
+        
+        jwt.verify(token, privateKey, {algorithm: 'HS256'}, (err, decoded) => {
+            if (err) {
+                res.status(500).json({ error:"Not Authorized"});
+            }
+            console.log(decoded);
+
+            return next();
+        });
+    } else {
+        res.status(500).json({error: "Not Authorized"});
+    }
+}
 
 var server = app.listen(3000, function() {
   var host = server.address().address;
