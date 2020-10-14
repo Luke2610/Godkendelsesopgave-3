6e2c17397e0f82f1a0cc81d049ebbe10aea300bc
Lucas <luve20ac@student.cbs.dk>
2020-10-14 17:12:07 +0200

    endpoints updated with CRUD

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-class user{
+
+class user{ // class user
     constructor(firstName, lastName, age, gender, interest, match,image){
     this.firstName = firstName;
     this.lastName = lastName;
@@ -9,7 +10,7 @@ class user{
     this.image = image;
     }
 }
-
+// class paymentUser with inheritance from class user
 class paymentUser extends user{
     constructor(firstName,lastName,age,gender,interest,match, image,creditCard) {
         super(firstName,lastName,age,gender,interest,match, image)
@@ -17,6 +18,7 @@ class paymentUser extends user{
     }
 }
 
+// class freeUser with inheritance from class user
 class freeUser extends user{
         constructor(firstName,lastName,age,gender,interest,match,image, creditCard) {
             super(firstName,lastName,age,gender,interest,match,image)
@@ -24,53 +26,83 @@ class freeUser extends user{
     }
 }
 
-var kasper = new paymentUser("Kasper","Jakobsen","21","male","female",22, true,true);
+// the users
+var kasper = new paymentUser("Kasper","Jakobsen","21","male",["Gaming","Food","Scout"],22, true,true);
 console.log(kasper)
 
-var eva = new freeUser("Eva","Hansen","20","female", "male",3,true,false)
+var eva = new freeUser("Eva","Hansen","20","female", ["Communism","Fishing",""],3,true,false)
 console.log(eva)
 
 
 
-// everything under this line is Express to make the server run
+// everything under this line is the server stuff
 var express = require("express");
 var app = express();
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
-const bodyParser = require('body-parser')
-const { stringify } = require("querystring");
 
 app.get('/secret', isAuthorized, (req,res) => {
-    res.json({"message":"This is super secret, do not share!" + stringify(kasper)})
+    res.json({"message":"This is super secret, do not share!"})
 })
 
-app.get('/readme', (req,res) => {
-    res.json({"message": "This is open to the world!" + JSON.stringify(kasper)})
-})
 app.get('/jwt',(req,res) => {
     let privateKey = fs.readFileSync('./private.pem','utf8');
-    let token = jwt.sign({"body": "stuff"}, privateKey ,{algorithm: 'HS256'});
+    let token = jwt.sign({"Authorization": "Authorized"}, privateKey ,{algorithm: 'HS256'});
     res.send(token);
-})
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + "/index.html");
 })
 
 //CRUD-Endpoints for User, Interest & Match.
-app.use(bodyParser.urlencoded({ extended: true}))
+
 //User-endpoint
-app.post('/user', (req,res) => {
-    console.log(req.body)
+app.get('/user', (req,res) => {
+    res.status(200).json(kasper)
+    console.log(req.body) //logging the forms from the HTML
 })
 
 //Interest-endpoint
+// Read
 app.get('/interest', (req,res) => {
-    res.json(kasper.interest);
+    res.send("Kaspers interests are: " + kasper.interest);
+});
+
+// create new interest for user (I use Postman to do the post request)
+app.post('/interest', (req,res) => {
+    newInterest = "Running";
+    kasper.interest.push(newInterest);
+    res.status(201).json(newInterest);
+});
+
+app.put('/interest', function (req, res) {
+    updatedInterest = "Jumping";
+    kasper.interest.splice(3,1,updatedInterest)
+    res.sendStatus(204)
+});
+
+app.delete('/interest', (req,res) => {
+    kasper.interest.splice(2,1)
+    res.sendStatus(204);
 })
 
 //Match-endpoint
 app.get('/match', (req,res) => {
-    res.json("Number of matches: " + kasper.match);
+    res.send("Kaspers number of matches: " + kasper.match);
+})
+
+app.post('/match', (req,res) => {
+    newMatches = 32; // new number of matches
+    kasper.match = newMatches;
+    res.status(201).json(newMatches);
+});
+
+app.put('/match', function (req, res) {
+    newMatches = 12; // new matches
+    kasper.match = kasper.match + newMatches; //current matches + new matches
+    res.sendStatus(204);
+});
+
+app.delete('/match', (req,res) => {
+    kasper.match = 0;
+    res.sendStatus(204);
 })
 
 function isAuthorized(req, res, next){ // this function gives access with the JWT.
